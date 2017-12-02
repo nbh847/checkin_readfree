7c61f5eba53d514b544be1fa7a77673cff54c05c
binhui <binhui.ni@yourdream.cc>
2017-12-02 18:55:38 +0800

    delete pwd

--- config/setting.py
+++ config/setting.py
@@ -11,11 +11,12 @@ MYSQL_DB_NAME = 'checkin'
 MYSQL_HOST = '127.0.0.1'
 MYSQL_PORT = 3306
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = '847nibinhui'
+MYSQL_PASSWORD = 'xxxxxxxxx'
 MYSQL_CHARSET = 'utf8'
 
 # Redis config
 REDIS_HOST = '127.0.0.1'
 REDIS_PORT = 6379
 REDIS_DB_ID = 0
+
 
