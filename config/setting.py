a6b94690e41af62b17f5b79bce941536617c7170
binhui <binhui.ni@yourdream.cc>
2017-12-02 18:57:47 +0800

    fix something

--- config/setting.py
+++ config/setting.py
@@ -1,7 +1,7 @@
 # config file
 
 USERAGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
-COOKIES = 'sessionid=tdw1ta34zbrzuoyooton0tg3iuio9ul4; csrftoken=keSFfxuM6PN5eiwSPksPyD9268upnYRrIVML6v853iO8AEcPHEqlOfFcqc3LKgSC'
+COOKIES = 'sss'
 HOST = 'readfree.me'
 RETRY_TIMES = 6
 MY_DEBUG_LEVEL = 'debug'
@@ -11,7 +11,7 @@ MYSQL_DB_NAME = 'checkin'
 MYSQL_HOST = '127.0.0.1'
 MYSQL_PORT = 3306
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'xxxxxxxxx'
+MYSQL_PASSWORD = '847nibinhui'
 MYSQL_CHARSET = 'utf8'
 
 # Redis config
