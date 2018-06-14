ec2a8a2b6a7c5de12d45a240df7ae7b9d07ed8a6
weasny <weasny@weasnydeMacBook-Pro.local>
2018-06-14 14:51:50 +0800

    append time formation to logger

--- logger_setting.py
+++ logger_setting.py
@@ -6,6 +6,10 @@ from logging import basicConfig, getLogger
 def get_logger():
     level_dic = {'error': logging.ERROR, 'info': logging.INFO, 'warning': logging.WARNING,
                  'debug':logging.DEBUG, 'warn':logging.WARN}
-    basicConfig(level=level_dic[MY_DEBUG_LEVEL])
+    basicConfig(
+        level=level_dic[MY_DEBUG_LEVEL],
+        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+        datefmt='%a, %Y-%m-%d %H:%M:%S',
+    )
     logger = getLogger(__name__)
     return logger
